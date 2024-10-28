8f318af756f67a4c431d33bd08b6f15c71c2f610
Marian Leontiev <leontievmarian@gmail.com>
2024-10-28 23:43:32 +0100

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Marian Leontiev on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
